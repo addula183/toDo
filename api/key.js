57cfed840e2b3d432421a457f24555400a785cbd
addula183 <167561698+addula183@users.noreply.github.com>
2024-04-23 14:22:20 -0500

    initial commit

---
+++ api/key.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://addulayaswanthreddy:abcd@cluster0.l9vhkku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+   
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
+
+
